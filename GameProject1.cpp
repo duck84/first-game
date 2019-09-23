82fd3e4839f5cad3574a7dad9e1b728218fb7b24
Mike <mmcgrath@psu.edu>
2019-09-22 23:24:00 -0700

    adding triangle code

--- GameProject1.cpp
+++ GameProject1.cpp
@@ -76,6 +76,81 @@ GLint LoadShader(GLenum type, const char *shaderSrc)
 	glCompileShader(TheShader);
 	
 	GLint IsItCompiled;
-	
-	
+
+// After the compile we need to check the status and report any errors
+	glGetShaderiv(TheShader, GL_COMPILE_STATUS, &IsItCompiled);
+	if (!IsItCompilied)
+	{
+		GLint RetinfoLen = 0;
+		glGetShaderiv(TheShader, GL_INFO_LOG_LENGTH, &RetinfoLen);
+		if (RetinfoLen > 1)
+		{
+			char* infoLog = (char*) malloc(sizeof(char)) * RetinfoLen);
+			glGetShaderInfoLog(TheShader, RetinfoLen, NULL, infoLog);
+			fprintf(stderr, "Error compiling this shader:\n%s\n", infoLog);
+			free(infoLog); 
+		}
+		glDeleteShader(TheShader);
+		return 0;
+	}
+	return TheShader;	
+}
+
+// Initialize the shader and program object
+int Init(Target_State *p_state)
+{
+	p_state->user_data = (UserData*)malloc(sizeof(UserData));
+	GGbyte vShaderStr[] =
+			"attribute vec4 a_position;\n"
+			"attribute vec2 a_texCoord;\n"
+			"varying vec2 v_texCoord;\n"
+			"void main()\n"
+			"{gl_Position=a_position;\n"
+			" vtextCoord=a_texCoord;}\n";
+
+	GLbyte fShaderStr[] = 
+			"precision mediump float;\n"
+			"varying vec2 v_texCoord;\n"
+			"uniform sampler2d s_texture;\n"
+			"void main()\n"
+			"{gl_FragColor=vec4 (1.0,0.0,0.0,1.0);}\n";
+
+	GLuint programObject, vertexShader, fragmentShader; // we need some variables
+
+// Load and compile the vertex/fragment shaders
+	vertexShader = LoadShader(GL_VERTEX_SHADER, (char*)vShaderStr);
+	fragmentShader = LoadShader(GL_FRAGMENT_SHADER, (char*)fShaderStr);
+
+// Create the program object
+	programObect = glCreateProgram();
+	if(programObject == 0) return 0;
+
+// Now we have the V and F shader attach them to the program object
+	glAttachShader(programObject, vertexShader);
+	glAttachShader(programObject, fragmentShader);
+
+// Link the program
+	glLinkProgram(programObject);
+// Check the link status
+	GLint AreTheylinked;
+	glGetProgramiv(programObject, GL_LINK_STATUS, &AreTheylinked);
+	if(!AreTheylinked)
+	{
+		GLint RetinfoLen = 0;
+// Check and report any errors
+		glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &RetinfoLen);
+		if(RetinfoLen > 1)
+		{
+			GLchar* infoLog = (GLchar*)malloc(sizeof(char) * RetinfoLen);
+			glGetProgramInfoLog(programObject, RetinfoLen, NULL, infoLog);
+			fprintf(stderr, "Error linking program:\n%s\n", infoLog);
+			free(infoLof);
+		}
+		glDeleteProgram(programObject);
+		return FALSE;
+	}
+	// Store the program object
+	p_state->user_data->programObject = programObject;
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	return TRUE;
 }
